4bd5c1f48d3de306ea588f0d2aef44691aa8802b
Jirrer <jrirrer@gmail.com>
2025-09-11 18:00:59 -0400

    created email funcionality

--- Backend/SendEmailReport.py
+++ Backend/SendEmailReport.py
@@ -1,18 +1,43 @@
 import sqlite3
+from dotenv import load_dotenv
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
 
+load_dotenv()
 
 def sendReports(userName):
     userInformation = pullUserInformation(userName) 
-    userEmail = userInformation[0]
+    userEmail, userScreenTime = userInformation[0], userInformation[1]
 
-    #send email here
+    scriptUserName = os.getenv("EMAIL_USERNAME")
+    scriptPassword = os.getenv("EMAIL_PASSWORD")
 
-    print(userInformation)
+    message = generateEmailMessage(userScreenTime)
+
+    msg = MIMEMultipart()
+    msg["Subject"] = f"Screen Time Report"
+    msg["To"] = userEmail
+    msg["From"] = scriptUserName
+
+    msg.attach(MIMEText(message))
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(scriptUserName, scriptPassword)
+        server.send_message(msg)
+
 
 def pullUserInformation(userName): # need to pull screen time too
     with sqlite3.connect('Backend/ClearReportDB.db') as connection:
         cursor = connection.cursor()
-        cursor.execute("SELECT email FROM users WHERE username = ?", (userName,))
+        cursor.execute("SELECT email, screenTime FROM users WHERE username = ?", (userName,))
         return cursor.fetchone()
+    
+def generateEmailMessage(screenTime):
+    message = f'This is a test message.\n You had {screenTime} hrs of screen time.'
+
+    return message
 
 sendReports('admin 1')
